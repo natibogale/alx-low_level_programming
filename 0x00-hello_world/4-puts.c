b303f1aa05bcb53d889615b4582bbef0c564c1bf
Nathanael <natibogale45@gmail.com>
2021-08-26 22:00:37 +0300

    done with all

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+*
+*this is a code that prints using puts
+*
+*/
+int main(void)
+{
+	int puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ *main prints the size of each type
+ *
+ *returns 0 if correct
+ */
+int main(void)
+{
+	int i;
+	char c;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %d byte(s) \n", sizeof(c));
+	printf("Size of an int: %d byte(s) \n", sizeof(i));
+	printf("Size of a long int: %d byte(s) \n", sizeof(li));
+	printf("Size of a long long int: %d byte(s) \n", sizeof(lli));
+	printf("Size of a float: %d byte(s) /n", sizeof(f));
+
+	return(0);
+}
